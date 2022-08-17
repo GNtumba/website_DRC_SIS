fda920beea54e15c2c64a1fc0837b678c8e495eb
Christian KASSE <gadtshimama@gmail.com>
2022-08-17 14:06:26 +0100

    the commit:fixed formulary

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('fonctions.php');
-?>
-<form action="#" method="POST">
-    <input name="nom" type="text" placeholder="Ecrire le nom"><br>
-    <input name="ok" value="ok" type="submit">
-</form>
